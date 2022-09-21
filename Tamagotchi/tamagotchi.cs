12eef6c35952727d2d0f95ecb8e5959b565baf83
Rayan Hadjari <rayan.ha@me.com>
2022-09-21 15:01:22 +0200

    soon done

--- Tamagotchi/tamagotchi.cs
+++ Tamagotchi/tamagotchi.cs
@@ -1,46 +1,53 @@
 using System;
 
-public class tamagotchi
+public class Tamagotchi
 {
     int hunger;
     int boredom;
-    List<string> words; 
+    List<string> words = new List<string>() {"Hi!"}; 
     bool isAlive;
     Random generator;
     public string name;
 
-    public tamagotchi(int hunger, int boredom, List<string> words, bool isAlive, Random generator, string name){
-        this.hunger = hunger;
-        this.boredom = boredom;
-        this.words = words;
-        this.isAlive = isAlive;
-        this.generator = generator;
-        this.name = name;
+    public Tamagotchi(){
+        this.isAlive = true;
+        this.generator = new Random();
     }
     public void Feed(){
-
+        hunger -= 2;
+        if (hunger < 0) {
+            hunger = 0;
+        }
     }
     public void Hi(){
-
+        int wordNum = generator.Next(words.Count);
+        Console.WriteLine(name + " says: " + words[wordNum]);
+        ReduceBoredom();
     }
-    public void Teach(){
-
+    public void Teach(string word){
+        Console.WriteLine(name + " learned: " + word);
+        words.Add(word);
+        ReduceBoredom();
     }
     public void Tick(){
-        while(hunger < 10 || boredom < 10){
-           hunger++;
-           boredom++;
+        hunger++;
+        boredom++;
+        if(hunger >= 10 || boredom >= 10){
+            isAlive = false;
         }
-        isAlive = false;
     }
     public void PrintStats(){
-
+        Console.Write("\n" + "Name: " + name + "\n" + "Hunger: " + hunger + "\n" + "Boredom: " + boredom + "\n" + "Vocabulary: " + words.Count + "words" + "\n");
     }
     public bool GetAlive(){
-
+        return isAlive;
     }
     void ReduceBoredom(){
-
+        Console.Write("\n" + name + " is now less bored!");
+        boredom -= 2;
+        if (boredom < 0){
+            boredom = 0;
+        }
     }
     
 }
